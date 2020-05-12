af7680c930cde8f006b238b696b3e5559122049e
DreamEchoo100 <maze6572198@gmail.com>
2020-05-12 22:47:19 +0200

    Updating...

--- Current Date/app.js
+++ Current Date/app.js
@@ -7,3 +7,6 @@ const h2 = document.getElementById('dateh2');
 let currentDateh2 = new Date().toDateString();
 let dateh2 = document.createTextNode(currentDateh2);
 h2.appendChild(dateh2);
+
+const birthday = new Date(/*1980, 8, 4*/2020, 4, 12);
+console.log(birthday.getTime())
