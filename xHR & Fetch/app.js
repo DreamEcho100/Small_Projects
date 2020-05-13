d0fef1e47049582153f22cfe0ec5f64acca9d847
DreamEchoo100 <maze6572198@gmail.com>
2020-05-13 22:12:13 +0200

    Updating...

--- xHR & Fetch/app.js
+++ xHR & Fetch/app.js
@@ -19,10 +19,9 @@ console.log(xHR);
 
 //
 
-const btn = document.getElementById("btn");
-const url2 = "https://randomuser.me/api/";
+const btn1 = document.getElementById("btn1");
 
-btn.addEventListener("click", getInput);
+btn1.addEventListener("click", getInput);
 
 function getInput() {
 	fetch(url)
@@ -33,5 +32,33 @@ function getInput() {
 		console.log(data.results);
 		document.getElementById('div2').innerHTML = `${data.value} <br>`;
 		//<img src="${joke.icon_url}">
+	})
+}
+
+const url2 = "https://randomuser.me/api/";
+const elem = document.getElementById('input');
+const btn2 = document.getElementById("btn2");
+const output = document.getElementById('div3');
+
+elem.setAttribute('type', 'number');
+elem.setAttribute('value', 5);
+
+btn2.addEventListener('click', getUsers);
+
+function getUsers() {
+	//debugger;
+	let temp = `${url2}?results=${Number(elem.value)}`;
+	fetch(temp)
+	.then(function(rep){
+		return rep.json();
+	})
+	.then(function(data){
+		console.log(data.results);
+		let html = '';
+		for (let i = 0; i < data.results.length; i++) {
+			console.log(data.results[i]);
+			html += `${data.results[i].name.first} ${data.results[i].name.last}<br>`;
+		}
+		output.innerHTML = html;
 	})
 }
