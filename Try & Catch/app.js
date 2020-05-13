a4cbbfd7493051eb0ffd6657e721a4af58c82337
DreamEchoo100 <maze6572198@gmail.com>
2020-05-13 14:34:49 +0200

    Updating...

--- Try & Catch/app.js
+++ Try & Catch/app.js
@@ -13,14 +13,15 @@ function tester() {
 	try {
 		let cases = ["no Value", "not a number", "over 5", "under 5"];
 		let testCase;
-		if (num === "") {h1.textContent = cases[0]; throw cases[0];};
-		if (isNaN(num)) {h1.textContent = cases[1]; throw cases[1];};
+		if (num === "") {h1.textContent = `"${num}" is ${cases[0]}`; throw cases[0];};
+		if (isNaN(num)) {h1.textContent = `"${num}" is ${cases[1]}`; throw cases[1];};
 		num = Number(num);
-		if (num > 5) {h1.textContent = cases[2]; throw cases[2];};
-		if (num < 5) {h1.textContent = cases[3]; throw cases[3];};
+		if (num > 5) {h1.textContent = `"${num}" is ${cases[2]}`; throw cases[2];};
+		if (num < 5) {h1.textContent = `"${num}" is ${cases[3]}`; throw cases[3];};
 	} catch (error) {
 		console.log(error);
 	} finally {
 		console.log("This will always show.");
+		inp.value = '';
 	}
 }
