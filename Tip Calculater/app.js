30748f2cbad49331cb0d0eaa28fbf8544e23a6e5
DreamEchoo100 <maze6572198@gmail.com>
2020-05-15 13:20:50 +0200

    Updating...

--- Tip Calculater/app.js
+++ Tip Calculater/app.js
@@ -1,8 +1,20 @@
-//const  let document querySelector querySelectorAll addEventListener keyCode textContent innerText getElementById style backgroundColor length color 
-const btn = document.querySelector("button");
-const output = document.querySelector(".output")
+//const  let document querySelector querySelectorAll addEventListener keyCode textContent innerText innerHTML getElementById style backgroundColor length color 
+const btn1 = document.querySelector(".button1");
+const output1 = document.querySelector(".output1");
+btn1.addEventListener("click", () => {
+	const inp = document.getElementById("input1");
+	let name = inp.value;
+	if (name !== "") {
+		output1.innerHTML = `<h1>Hello and welcome to our Homepage, ${name}.</h1>`;
+	}
+})
 
-btn. addEventListener("click", () =>{
+//
+const btn2 = document.querySelector(".button2");
+const output2 = document.querySelector(".output2");
 
-	output.innerText = "";
+btn2. addEventListener("click", () =>{
+	const cost = document.querySelector(".input2");
+	let tip = (cost.value * 0.15).toFixed(2);
+	output2.innerHTML = `<h1>You should tip $${tip} on $${cost.value}.</h1>`;
 })
