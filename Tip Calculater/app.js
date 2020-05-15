fb32887e24fdf692f5f82263853f90245ffe2f43
DreamEchoo100 <maze6572198@gmail.com>
2020-05-15 12:46:17 +0200

    Updating...

--- Tip Calculater/app.js
+++ Tip Calculater/app.js
@@ -1,1 +1,8 @@
-//document querySelector querySelectorAll addEventListener keyCode textContent getElementById style backgroundColor length color 
+//const  let document querySelector querySelectorAll addEventListener keyCode textContent innerText getElementById style backgroundColor length color 
+const btn = document.querySelector("button");
+const output = document.querySelector(".output")
+
+btn. addEventListener("click", () =>{
+
+	output.innerText = "";
+})
