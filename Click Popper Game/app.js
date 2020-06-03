b0e124e3d99209727ab73048cb5b362d75b3a4e4
DreamEchoo100 <maze6572198@gmail.com>
2020-06-03 18:24:13 +0200

    Uploading&Updating...

--- Click Popper Game/app.js
+++ Click Popper Game/app.js
@@ -1,10 +1,6 @@
 const playArea = {};
 const player = {};
 let gameObj;
-let gameScores = [250, 200, 150, 100, 50, 25, 10, 5, -250, -200, -150, -100, -50, -25, -10, -5]
-
-player.score = 0;
-player.items = 3;
 
 playArea.stats = document.querySelector(".stats");
 playArea.main = document.querySelector(".main");
@@ -12,29 +8,14 @@ playArea.game = document.querySelector(".game");
 playArea.btns = Array.from(document.querySelectorAll(".btn"));
 playArea.page = Array.from(document.querySelectorAll(".page"));
 
-console.log(playArea);
-
-console.log(player);
+player.score = 0;
+player.items = 3;
+player.highestScore = localStorage.getItem("highestScoreInClickPopperMiniGame") || 0;
 
 document.addEventListener("DOMContentLoaded", getData);
 
-playArea.btns.forEach(function(item) {
-	item.addEventListener("click", handleBtn);
-});
-
 function getData() {
-	
-
 	playArea.main.classList.add("visible"); 
-	/*fetch(`https://api.adorable.io/avatars/83/${word}@adorable.io.png`)
-	.then(function(rep){
-		console.log(rep.json());
-        return rep.json();
-    }).then(function(data){
-        gameObj = data.data;
-        console.log(gameObj);
-	buildBoard();
-    })*/
 	if (!localStorage.getItem("faces")) {
 		let chars = ["f", "u", "c", "k"];
 		let faces = [];
@@ -55,28 +36,61 @@ function getData() {
 	buildBoard();
 }
 
+function buildBoard() {
+	playArea.scorer = document.createElement("span");
+    playArea.scorer.innerHTML = "Press Button to Start";
+    playArea.stats.appendChild(playArea.scorer);
+	let rows = 4;
+	let cols = 4;
+	let cnt = 0;
+	playArea.game.style.width = cols * 100 + (cols * 2);
+	//playArea.game.style.margin = "auto";
+	playArea.game.style.width = "fit-content";
+	for (let i = 0; i < rows; i++) {
+		let divMain = document.createElement("div");
+		divMain.setAttribute("class", "row");
+		divMain.style.width = cols * 100  + (cols * 2);
+		for (let j = 0; j < cols; j++) {
+			let div = document.createElement("div");
+			div.setAttribute("class", "pop");
+			cnt++;
+			div.innerText = cnt;
+			div.cnt = cnt;
+			div.classList.add("gameCell");
+			divMain.appendChild(div);
+		}
+		divMain.classList.add("gameCell");
+		divMain.style.width = "fit-content";
+		playArea.game.appendChild(divMain);
+	}
+}
+
+playArea.btns.forEach(function(item) {
+	item.addEventListener("click", handleBtn);
+});
+
 function handleBtn(e) {
-	console.log(e.target.classList.contains("newGame"));
 	if (e.target.classList.contains("newGame")) {
-		console.log("start");
 		startGame();
 	}
 }
 
 function startGame() {
-	console.log(player);
 	playArea.main.classList.remove("visible");
 	playArea.game.classList.add("visible");
-	console.log("Start");
 	player.gameOver = false;
-	let gc = document.querySelectorAll(".pop");
-	for (let i = 0; i < gc.length; i++) {
-	    gc[i].addEventListener("click", function() {
-	    	player.score = player.score + gc[i].v;
-	    	gc[i].v = 0;
-	        gc[i].innerHTML = gc[i].cnt;
-	        gc[i].classList.remove("active");
-	        gc[i].removeEventListener("click", hitPop);
+	let wouldPop = document.querySelectorAll(".pop");
+	for (let i = 0; i < wouldPop.length; i++) {
+	    wouldPop[i].addEventListener("click", function() {
+	    	player.score = player.score + wouldPop[i].v;
+	    	wouldPop[i].v = 0;
+	        wouldPop[i].innerHTML = wouldPop[i].cnt;
+	        wouldPop[i].classList.remove("active");
+	        wouldPop[i].removeEventListener("click", hitPop);
+	        if(player.score > player.highestScore) {
+			    localStorage.setItem("highestScoreInClickPopperMiniGame", player.score);
+			    player.highestScore = player.score;
+			}
 	        if(player.score < 0) {
 			    player.items = Number(player.items) - 1;
 			    player.score = 0;
@@ -86,43 +100,35 @@ function startGame() {
 			}
 	    	updateScore();
 	    })
-	    gc[i].v = 0;
-	    gc[i].innerHTML = gc[i].cnt;
-	    gc[i].classList.remove("active");
-	    gc[i].removeEventListener("click", hitPop);
-	    console.log("work")
+	    wouldPop[i].v = 0;
+	    wouldPop[i].innerHTML = wouldPop[i].cnt;
+	    wouldPop[i].classList.remove("active");
+	    wouldPop[i].removeEventListener("click", hitPop);
 	}
 	startPop();
     updateScore();
 }
 
-function cheker() {
-	if (player.score < 0) {
-		player.items = player.items--;
-		updateScore();
-	}
-	if (player.items < 0) {
-		gameOver();
-	}
-	updateScore();
-
+function startPop() {
+	let gameScores = [250, 200, 150, 100, 50, 25, 10, 5, -250, -200, -150, -100, -50, -25, -10, -5];
+	let newPop = randomUp();
+	newPop.classList.add("active");
+	newPop.addEventListener("click", hitPop);
+	const time = Math.round(Math.random() * (500) + 750);
+	const val = Math.floor(Math.random() * gameObj.length);
+	newPop.old = newPop.innerText;
+	let score = gameScores[val]
+	newPop.v = score;
+	newPop.innerHTML = `<img class="face" src="${gameObj[val]}"> <br> ${score}`;
+	playArea.inPlay = setTimeout(function () {
+        newPop.classList.remove("active");
+        newPop.removeEventListener("click", hitPop);
+        newPop.innerText = newPop.cnt;
+        if (!player.gameOver) {
+            startPop();
+        }
+    }, time);
 }
-//cheker();
-/*
-	let gc = document.querySelectorAll(".pop");
-	for (let i = 0; i < gc.length; i++) {
-	    gc[i].addEventListener("click", function() {
-	        gc[i].innerHTML = gc[i].cnt;
-	        gc[i].classList.remove("active");
-			gc[i].removeEventListener("click", hitPop);
-		})
-		console.log("work")
-		clearTimeout(playArea.inPlay);
-		if (!player.gameOver) {
-		    startPop();
-		}
-	}*/
-
 
 function randomUp() {
 	const pops = document.querySelectorAll(".pop");
@@ -134,49 +140,13 @@ function randomUp() {
 	return pops[idx];
 }
 
-function startPop() {
-	let newPop = randomUp();
-	console.log(newPop);
-	newPop.classList.add("active");
-    console.log(newPop.old)
-	newPop.addEventListener("click", hitPop);
-	const time = Math.round(Math.random() * (1500) + 750);
-	const val = Math.floor(Math.random() * gameObj.length);
-	newPop.old = newPop.innerText;
-	let score = gameScores[val]
-	newPop.v = score;
-	newPop.innerHTML = `<img class="img" src="${gameObj[val]}"> <br> ${score}`;
-	playArea.inPlay = setTimeout(function () {
-        newPop.classList.remove("active");
-        newPop.removeEventListener("click", hitPop);
-        newPop.innerText = newPop.cnt;
-        if (!player.gameOver) {
-            startPop();
-        }
-    }, time);
+function hitPop(e) {
+	let newPop = e.target;
+	updateScore();
 }
 
-function startPop() {
-    let newPop = randomUp();
-    console.log(newPop);
-    newPop.classList.add("active");
-    newPop.addEventListener("click", hitPop);
-    const time = Math.round(Math.random() * (250) + 500);
-    const val = Math.floor(Math.random() * gameObj.length);
-    newPop.old = newPop.innerText;
-	let score = gameScores[val]
-	
-	
-    newPop.v = score;
-    newPop.innerHTML = `<img class="img" src="${gameObj[val]}"> <br> ${score}`;
-    playArea.inPlay = setTimeout(function () {
-        newPop.classList.remove("active");
-        newPop.removeEventListener("click", hitPop);
-        newPop.innerText = newPop.old;
-        if (!player.gameOver) {
-            startPop();
-        }
-    }, time);
+function updateScore() {
+    playArea.scorer.innerHTML = `<span class="board">Score: ${player.score} Lives: ${player.items}<br>Highest Score: ${player.highestScore}</span>`;
 }
 
 function gameOver() {
@@ -186,45 +156,23 @@ function gameOver() {
     document.querySelector(".newGame").innerText = "Try Again";
     player.score = 0;
 	player.items = 3;
-    //document.querySelector(".board").innerText = "Try Again";
 }
 
-function hitPop(e) {
-	let newPop = e.target;
-	updateScore();
+/*
+{
+	"data":[{"icon": "\u0026#8902;", "value"10:},
+			{"icon": "\u0026#10031;", "value":30},
+			{"icon": "\u0026#10036;", "value":50},
+			{"icon": "\u0026#10042;", "value":70},
+			{"icon": "\u0026#10084;", "value":75},
+			{"icon": "\u0026#9813;", "value":50},
+			{"icon": "\u0026#9822;", "value":60},
+			{"icon": "\u0026#9924;", "value":40},
+			{"icon": "\u0026#9971;", "value":100},
+			{"icon": "\u0026#9729;", "value":-50},
+			{"icon": "\u0026#9785;", "value":-100},
+			{"icon": "\u0026#9760;", "value":-250},
+			{"icon": "\u0026#9791;", "value":-20},
+			]
 }
-
-
-function updateScore() {
-    playArea.scorer.innerHTML = `<span class="board">Score: ${player.score} Lives: ${player.items}</span>`;
-}
-
-function buildBoard() {
-	playArea.scorer = document.createElement("span");
-    playArea.scorer.innerHTML = "Press Button to Start";
-    playArea.stats.appendChild(playArea.scorer);
-	console.log("ready");
-	let rows = 4;
-	let cols = 4;
-	let cnt = 0;
-	playArea.game.style.width = cols * 100// + (cols * 2);
-	playArea.game.style.margin = "auto";
-	for (let i = 0; i < rows; i++) {
-		let divMain = document.createElement("div");
-		divMain.setAttribute("class", "row");
-		divMain.style.width = cols * 100 //+ (cols * 2);
-		for (let j = 0; j < cols; j++) {
-			let div = document.createElement("div");
-			div.setAttribute("class", "pop");
-			cnt++;
-			div.innerText = cnt;
-			div.cnt = cnt;
-			div.classList.add("gameCell");
-			console.log(div.cnt);
-			divMain.appendChild(div);
-		}
-		divMain.classList.add("gameCell");
-		divMain.style.width = "fit-content";
-		playArea.game.appendChild(divMain);
-	}
-}
+*/
