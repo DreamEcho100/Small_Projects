b47174d00153fb5948475686d791b6966dc5dd6d
DreamEchoo100 <maze6572198@gmail.com>
2020-06-22 18:50:29 +0200

    Upload&Update...

--- Coin Chaser/app.js
+++ Coin Chaser/app.js
@@ -1,33 +1,36 @@
 const player = document.querySelector("#player");
 const coin = document.querySelector("#coin");
+const score = document.querySelector("#score");
 
 let play = {
 	speed: (player.offsetWidth / 2), 
-	ready: true
+	ready: true,
+	score: -50
 }
 
-let keys = ["keydown", "keyhold"];
+let keys = ["keydown", "keyUp", "keyhold"];
 catchCoin(player, coin);
 
 keys.forEach((item) => {
     document.addEventListener(item, function (e) {
 	    if (play.ready) {
-	    	switch(e.key) {
-				case "ArrowUp":
-					player.style.top = `${(player.offsetTop) - play.speed}px`;
-					break;
-				case "ArrowDown":
-					player.style.top = `${(player.offsetTop) + play.speed}px`;
-					break;
-				case "ArrowLeft":
-					player.style.left = `${(player.offsetLeft) - play.speed}px`;
-					player.style.transform = "scale(-1, 1)";
-					break;
-				case "ArrowRight":
-					player.style.left = `${(player.offsetLeft) + play.speed}px`;
-					player.style.transform = "scale(1, 1)";
-					break;
+	    	let key = e.key;
+			if (key === "ArrowUp" && player.offsetTop > (score.offsetTop + score.offsetHeight)) {
+				player.style.top = `${(player.offsetTop) - play.speed}px`;
 			}
+			if (key === "ArrowDown") {
+				player.style.top = `${(player.offsetTop) + play.speed}px`;
+				if (player.offsetTop) {}
+			}
+			if (key === "ArrowLeft") {
+				player.style.left = `${(player.offsetLeft) - play.speed}px`;
+				player.style.transform = "scale(-1, 1)";
+			}
+			if (key === "ArrowRight") {
+				player.style.left = `${(player.offsetLeft) + play.speed}px`;
+				player.style.transform = "scale(1, 1)";
+			}
+			inTheGame(player);
 			player.ready = false;
 			setTimeout(() => {player.ready = true}, 500);
 	    }
@@ -35,23 +38,49 @@ keys.forEach((item) => {
     });
 })
 
+function inTheGame(item) {
+	// For getting out from the left
+	if (window.innerWidth < (item.offsetLeft + item.offsetWidth)) {
+	    item.style.left = `${window.innerWidth - item.offsetWidth}px`
+	}
+	// For getting out from the right
+	if ((item.offsetLeft) < 0) {
+	    item.style.left = `0px`
+	}
+	// For getting out from the top
+	if (!(item.offsetTop >  (score.offsetTop + score.offsetHeight))) {
+		item.style.top = `${score.offsetTop + score.offsetHeight + 1}px`;
+	}
+	// For getting out from the bottom
+	if ((item.offsetTop + item.offsetHeight) > window.innerHeight) {
+	    item.style.top = `${window.innerHeight - item.offsetHeight}px`;
+	}
+}
+
 function catchCoin(a, b) {
 	if (isCollide(a, b)) {
+		play.score += 50;
 		b.style.left = `${Math.floor(Math.random() * window.innerWidth - (window.innerWidth * 0.1))}px`;
 		b.style.top = `${Math.floor(Math.random() * window.innerHeight - (window.innerHeight * 0.1))}px`;
 		if (isCollide(a, b)) {
+			play.score -= 50;
 			catchCoin(a, b)
 		}
+		inTheGame(coin);/*
 		if (b.offsetLeft < 0) {
 			//Number(b.style.left.substr(0, b.style.left.length - 2)) < 0
 			let temp = Math.abs(b.offsetLeft);
 			b.style.left = `${temp}px`;
 		}
-		if (b.offsetTop < 0) {
+		if (b.offsetTop) {
 			//Number(b.style.top.substr(0, b.style.top.length - 2))
 			let temp = Math.abs(b.offsetTop);
 			b.style.top = `${temp}px`;
 		}
+		if (b.offsetTop < score.offsetTop + score.offsetHeight) {
+			b.style.top = `${score.offsetTop + score.offsetHeight + 1}px`
+		}*/
+		score.innerText = `Score: ${play.score}`;
 	}
 }
 
