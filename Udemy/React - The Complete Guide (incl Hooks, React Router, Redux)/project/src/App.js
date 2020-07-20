b8c4ce7849386a5582f511dd6ffce43cffdbacf6
DreamEchoo100 <maze6572198@gmail.com>
2020-07-20 18:43:01 +0200

    Upload&Update...

--- Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/project/src/App.js
+++ Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/project/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import cssModuleTst from './cssModuleTst.module.css';
 import Radium, {StyleRoot} from 'radium'
 import Assignment1 from './Assignment1'
 import Assignment2 from './Assignment2'
@@ -7,11 +8,13 @@ import Assignment2 from './Assignment2'
 class App extends React.Component  {
 
     render() {
+    	console.log(cssModuleTst.Tst, cssModuleTst)
 		return (
 			<StyleRoot>
 				<div className="App">
 					<Assignment1 />
 					<Assignment2 />
+					<p className={cssModuleTst.Tst}>Hello World</p>
 				</div>
 			</StyleRoot>
 		);
