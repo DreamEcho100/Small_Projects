e8cb1d178c712457b882757b21e2b9a653f2d753
DreamEchoo100 <maze6572198@gmail.com>
2020-07-21 00:14:10 +0200

    Upload&Update...

--- Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/project/src/Assignment1.js
+++ Udemy/React - The Complete Guide (incl Hooks, React Router, Redux)/project/src/Assignment1.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import InpOutField from './Assignments/Assignment1/InpOutField';
 
+const StyleDiv = styled.div`
+	border: 1px solid black;
+	margin: 5px;
+	padding: 5px,
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	background-color: lightblue;
+	text-align: center;
+	transition: all 0.5s;
+
+	&:hover {
+		border: 3px solid black;
+		margin: 3px;
+	}
+
+	@media (max-width: 980px) {
+		background-color: blue;
+	}
+
+	@media (max-width: 570px) {
+		background-color: red;
+	}
+
+	@media (max-width: 320px) {
+		background-color: yellow;
+	}
+`;
+
 class Assignment1 extends React.Component  {
 	state = {
 		inp: 'lol'
@@ -24,39 +53,10 @@ class Assignment1 extends React.Component  {
 */
     render() {
 
-    	const StyleDiv = styled.div`
-			border: 1px solid black;
-			margin: 5px;
-			padding: 5px,
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			background-color: lightblue;
-			text-align: center;
-			transition: all 0.5s;
-
-			&:hover {
-				border: 3px solid black;
-				margin: 3px;
-			}
-
-			@media (max-width: 980px) {
-				background-color: blue;
-			}
-
-			@media (max-width: 570px) {
-				background-color: red;
-			}
-
-			@media (max-width: 320px) {
-				background-color: yellow;
-			}
-    	`;
-
 		return (
 			<StyleDiv>
 				<InpOutField 
-					change={this.changeTheInp}
+					change={ (e) => this.changeTheInp(e) }
 					current={this.state.inp}
 				/>
 			</StyleDiv>
