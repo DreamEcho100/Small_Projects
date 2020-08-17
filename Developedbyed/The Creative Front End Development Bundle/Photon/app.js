f751c16e179249e9dcfdd1cb248d262a0e880138
DreamEchoo100 <maze6572198@gmail.com>
2020-08-17 22:04:45 +0200

    Upload&Update...

--- Developedbyed/The Creative Front End Development Bundle/Photon/app.js
+++ Developedbyed/The Creative Front End Development Bundle/Photon/app.js
@@ -1,1 +1,5 @@
 const pexelsAuth = "563492ad6f917000010000015821205f41f14bfa8e23d5e5b3734032";
+const gallery = document.querySelector(".gallery");
+const searchInp = document.querySelector(".search-inp");
+const submitBtn = document.querySelector(".submit-btn");
+let searchValue;
